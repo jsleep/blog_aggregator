bc0e1350928f54b92c5eaab77cf099f56d9f842f
jsleep <jonrsleep@gmail.com>
2025-03-07 10:59:42 -0800

    database commands

--- go.mod
+++ go.mod
@@ -3,4 +3,10 @@ module github.com/jsleep/blog_aggregator
 go 1.23.5
 
 require internal/config v1.0.0
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
+
 replace internal/config => ./internal/config
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"internal/config"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jsleep/blog_aggregator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type command struct {
@@ -16,6 +23,9 @@ type command struct {
 type state struct {
 	// Config is the configuration
 	Config *config.Config
+
+	// db is the database connection
+	db *database.Queries
 }
 
 type commands map[string]func(*state, command) error
@@ -32,7 +42,7 @@ func (c *commands) run(s *state, cmd command) error {
 	return handler(s, cmd)
 }
 
-func handlerLogin(s *state, cmd command) error {
+func loginHandler(s *state, cmd command) error {
 	// Check if the command is "login"
 	if cmd.Command != "login" {
 		return fmt.Errorf("invalid command")
@@ -40,16 +50,96 @@ func handlerLogin(s *state, cmd command) error {
 
 	// Check if the arguments are valid
 	if len(cmd.Args) < 1 {
-		return fmt.Errorf("missing arguments")
+		return fmt.Errorf("missing name argument")
 	}
 
-	// Set the user in the configuration
-	err := s.Config.SetUser(cmd.Args[0])
+	name := cmd.Args[0]
+
+	user, err := s.db.GetUser(context.Background(), name)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("User set to %s\n", cmd.Args[0])
+	// Set the user in the configuration
+	err = s.Config.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User set to %s\n", user.Name)
+
+	return nil
+}
+
+func resetHandler(s *state, cmd command) error {
+	// Check if the command is "login"
+	if cmd.Command != "reset" {
+		return fmt.Errorf("invalid command")
+	}
+
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func listUsersHandler(s *state, cmd command) error {
+	// Check if the command is "login"
+	if cmd.Command != "users" {
+		return fmt.Errorf("invalid command")
+	}
+
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		current := ""
+		if user.Name == s.Config.User {
+			current = " (current)"
+		}
+		fmt.Printf("* %s%s\n", user.Name, current)
+	}
+
+	return nil
+}
+
+func registerHandler(s *state, cmd command) error {
+	// Check if the command is "register"
+	if cmd.Command != "register" {
+		return fmt.Errorf("invalid command")
+	}
+
+	// Check if the arguments are valid
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("missing name arguments")
+	}
+
+	name := cmd.Args[0]
+
+	// Register the user in the database
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		Name:      name,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User %s registered successfully\n", user.Name)
+
+	err = s.Config.SetUser(user.Name)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User data  %s\n", user)
 
 	return nil
 }
@@ -64,8 +154,20 @@ func main() {
 		Config: &cfg,
 	}
 
+	db, err := sql.Open("postgres", cfg.DBUrl)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+	state.db = dbQueries
+
 	commands := make(commands)
-	commands.register("login", handlerLogin)
+	commands.register("login", loginHandler)
+	commands.register("register", registerHandler)
+	commands.register("reset", resetHandler)
+	commands.register("users", listUsersHandler)
 
 	args := os.Args
 	if len(args) < 2 {
@@ -80,7 +182,7 @@ func main() {
 
 	err = commands.run(state, cmd)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 	fmt.Println("Command executed successfully")
